2305b3e3ad026fc057e82c7d7a4b784b584c6740
sotesneal <nsotes@fullscale.io>
2022-07-22 17:48:17 +0800

    Implement ICommandHandler interface

---
+++ WiredBrainCoffee.ShopInfoToolNew/CoffeeCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.ShopInfoToolNew
+{
+    internal class CoffeeCommandHandler : ICommandHandler
+    {
+        private IEnumerable<CoffeeShop> coffeeShops;
+        private string? line;
+
+        public CoffeeCommandHandler(IEnumerable<CoffeeShop> coffeeShops, string? line)
+        {
+            this.coffeeShops = coffeeShops;
+            this.line = line;
+        }
+
+        public void HandleCommand()
+        {
+            var foundCoffeeShops = coffeeShops
+               .Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (foundCoffeeShops.Count == 0)
+            {
+                Console.WriteLine($"> Command '{line}' not found");
+            }
+            else if (foundCoffeeShops.Count == 1)
+            {
+                var coffeeShop = foundCoffeeShops.Single();
+                Console.WriteLine($"> Location: {coffeeShop.Location}");
+                Console.WriteLine($"> Beans in stock: {coffeeShop.BeansInStockInKg} Kg");
+            }
+            else
+            {
+                Console.WriteLine($"> Multiple matching coffee shop commands found: ");
+                foreach (var coffeeType in foundCoffeeShops)
+                {
+                    Console.WriteLine($"> {coffeeType.Location}");
+                }
+            }
+        }
+    }
+}
--- WiredBrainCoffee.ShopInfoToolNew/Program.cs
+++ WiredBrainCoffee.ShopInfoToolNew/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Linq;
 using WiredBrainCoffee.DataAccess;
+using WiredBrainCoffee.ShopInfoToolNew;
 
 Console.WriteLine("Hello, World!");
 
@@ -16,38 +17,11 @@ while (true)
     {
         break;
     }
-    if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
-    {
+    ICommandHandler commandHandler = string.Equals("help", line, StringComparison.OrdinalIgnoreCase) ?
+    new HelpCommandHandler(coffeeShops) as ICommandHandler
+    : new CoffeeCommandHandler(coffeeShops, line);
 
-        Console.WriteLine("> Available coffee shop commands: ");
-        foreach (var coffeeShop in coffeeShops)
-        {
-            Console.WriteLine($"> " + coffeeShop.Location);
-        }
-    }
-    else
-    {
-        var foundCoffeeShops = coffeeShops
-            .Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase)).ToList();
+    commandHandler.HandleCommand();
 
-        if (foundCoffeeShops.Count == 0)
-        {
-            Console.WriteLine($"> Command '{line}' not found");
-        }
-        else if (foundCoffeeShops.Count == 1)
-        {
-            var coffeeShop = foundCoffeeShops.Single();
-            Console.WriteLine($"> Location: {coffeeShop.Location}");
-            Console.WriteLine($"> Beans in stock: {coffeeShop.BeansInStockInKg} Kg");
-        }
-        else
-        {
-            Console.WriteLine($"> Multiple matching coffee shop commands found: ");
-            foreach (var coffeeType in foundCoffeeShops)
-            {
-                Console.WriteLine($"> {coffeeType.Location}");
-            }
-        }
-    
-    }
+
 }
